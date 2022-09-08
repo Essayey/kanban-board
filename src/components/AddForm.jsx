2c2057cce14285ce184e9e3e9fd68429b00597d3
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-08 19:12:56 +0300

    Refactored AddForm component

---
+++ src/components/AddForm.jsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useEscapeCallback, useOutsideCallback } from '../hooks/useOutsideCallback';
+import { doAddCard } from '../store';
+import { resizeTextArea, submitOnEnter } from '../utils';
+
+const AddForm = ({
+    buttonText,
+    placeholder,
+    closeFormCallback,
+    callback,
+    listScrollRef
+}) => {
+    const [value, setValue] = useState('');
+
+    const textAreaRef = useRef();
+    const formRef = useRef();
+
+    const onCloseForm = () => {
+        console.log('test')
+        closeFormCallback();
+        setValue('');
+    }
+
+    useOutsideCallback(onCloseForm, formRef);
+    useEscapeCallback(onCloseForm);
+
+    const submit = e => {
+        e.preventDefault();
+        if (value !== '') {
+            callback(value);
+            setValue('');
+            textAreaRef.current.style = 'height: auto';
+        }
+    }
+
+    const onInput = e => {
+        setValue(e.target.value);
+        resizeTextArea(textAreaRef);
+        if (listScrollRef) listScrollRef.current.scrollTop = listScrollRef.current.scrollHeight;
+    }
+
+    return (
+        <form ref={formRef} onSubmit={submit}>
+            <textarea
+                placeholder={placeholder}
+                ref={textAreaRef}
+                autoFocus
+                value={value}
+                onChange={onInput}
+                onKeyPress={e => submitOnEnter(e, formRef)}
+                type="text"
+                className='input' />
+            <div className='Card__buttons'>
+                <button type='submit' className='btn'>{buttonText}</button>
+                <div onClick={onCloseForm} className='closeIcon'></div>
+            </div>
+        </form>
+    )
+}
+
+export default AddForm

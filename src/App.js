cde6fac41952fc14551c12e295f9e319137aea59
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-09 19:23:47 +0300

    Added nested routing
    Added nested routing to isolate CardModal componen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { doAddCard, doAddList, doCreateBoard, doDeleteBoard, doDeleteCard, doDeleteList, doEditCard, doRenameBoard, doRenameList } from './store';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Board from './components/Board';
+import CardModal from './components/CardModal';
 
 function App() {
     const dispatch = useDispatch();
     const boards = useSelector(state => state.boards);
     const boardsNames = boards.map(board => board.name);
+    const boardRef = useRef();
 
     const createBoard = boardName => {
         dispatch(doCreateBoard({ boardName }));
@@ -40,7 +42,13 @@ function App() {
             <BrowserRouter>
                 <Sidebar boards={boardsNames} />
                 <Routes>
-                    <Route path='boards/:id' element={<Board board={boards[0]} />} />
+                    <Route path='boards/:boardId' element={<Board board={boards[0]} />}>
+                        <Route path=':listId/:cardId' element={<CardModal
+
+                            title='Card 1'
+                            text='Lorem ipsum dolor sit amet, 
+            consectetur adipisicing elit. Voluptates, nesciunt!'/>} />
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </div>

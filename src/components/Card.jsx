577c3e7098ce20fad7c2fa0db3031a9c1b17afca
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-10 23:48:43 +0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,47 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import CardPopup from './CardPopup';
+import React, { useRef, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { doEditCardTitle } from '../store';
+import CardContextMenu from './CardContextMenu';
+
 
 const Card = ({ listId, cardId, card }) => {
+    const dispatch = useDispatch();
+    const { boardId } = useParams();
     const [isEditing, setIsEditing] = useState(false);
+
+    const editTitle = newText => {
+        dispatch(doEditCardTitle({
+            boardId: Number(boardId),
+            listId: Number(listId),
+            cardId: Number(cardId),
+            newText: newText
+        }))
+    }
 
     const onContextMenu = e => {
         e.preventDefault()
         setIsEditing(true);
     }
-
     return (
-        <div>
+        <div className='Card'>
             <Link onContextMenu={onContextMenu} class={'Link-normalize'} to={`${listId}/${cardId}`}>
-                <div className='Card'>
+                <div>
                     {card.title}
                 </div>
             </Link>
             {
                 isEditing
-
-                    ? <CardPopup
-
+                    ? <CardContextMenu
+                        boardId={boardId}
+                        listId={listId}
+                        cardId={cardId}
+                        initialValue={card.title}
+                        buttonText={'Rename'}
+                        closeFormCallback={() => setIsEditing(false)}
+                        callback={editTitle}
                     />
                     : null
-
             }
 
         </div >

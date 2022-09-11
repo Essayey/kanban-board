d6865d2085a09c78ab8cba66c06cef4f4fac8826
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-11 16:40:51 +0300

    Add: Close context menu using React.forwwardRef

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ContextMenu = ({ children, left, top }) => {
+const ContextMenu = React.forwardRef(({ children, left, top }, ref) => {
     return (
-        <div style={{ left: left, top: top }} className='ContextMenu'>
+        <div ref={ref} style={{ left: left, top: top }} className='ContextMenu'>
             <div className='ContextMenu__inner'>
                 {children}
             </div>
         </div>
     )
-}
+})
 
 export default ContextMenu

c1608c222cbec6b2146356b4aabaddbf119ef194
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-11 16:40:15 +0300

    Update Button.jsx

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Button = ({ className, onClick, children, variant }) => {
     const color = variant === 'primary' ? '#5757ff' : '#111111aa'
-    console.log(color)
     return (
         <button onClick={onClick} style={{ background: color }} type='submit' className={className + ' btn'}>
             {children}

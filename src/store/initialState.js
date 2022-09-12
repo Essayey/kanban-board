e4671c00e72081aaad19663fed192486aae45b96
Essayey <47187299+Essayey@users.noreply.github.com>
2022-09-12 18:32:37 +0300

    Add: DevState
    Titles of the cards is numbers

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -3,18 +3,109 @@ export const initialState = {
         name: 'Board #1',
         lists: [{
             cards: [{
-                title: '1',
+                title: 'Welcome to Kanban Board :)',
+                description: 'Learn Redux thunk'
+            },
+            {
+                title: 'You can click this card to edit',
+                description: 'Build server and database'
+            },
+            {
+                title: 'You can Drag and Drop this card.',
+                description: ''
+            }, {
+                title: 'Also you can right click cards to open context menu',
                 description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            }],
+            listName: "Todo"
+        }, {
+            cards: [{
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },],
+            listName: "Doing"
+        }, {
+            cards: [{
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },],
+            listName: "Done"
+        }]
+    },
+    {
+        name: 'Board #2',
+        lists: [{
+            cards: [{
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quisquam.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quisquam.',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            }],
+            listName: "List 1"
+        }]
+    }
+    ],
+    dropCardState: {
+        boardId: 0,
+        srcCardId: 0,
+        srcListId: 0,
+        destCardId: 0,
+        destListId: 0
+    }
+}
+
+
+export const initialStateDev = {
+    boards: [{
+        name: 'Board #1',
+        lists: [{
+            cards: [{
+                title: '1',
+                description: 'Learn Redux thunk'
             },
             {
                 title: '2',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+                description: 'Build server and database'
             },
             {
                 title: '3',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+                description: ''
             },],
-            listName: "List 1"
+            listName: "Todo"
         }, {
             cards: [{
                 title: '4',
@@ -36,7 +127,29 @@ export const initialState = {
                 title: '8',
                 description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
             },],
-            listName: "List 1"
+            listName: "Doing"
+        }, {
+            cards: [{
+                title: '4',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: '5',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: '6',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: '7',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                title: '8',
+                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+            },],
+            listName: "Done"
         }]
     },
     {
